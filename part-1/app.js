8bd0a7ea901e0f8047132f8caf4e75a7c9ac9dbd
SAL <solomanasoloman@gmail.com>
2017-10-24 08:11:28 -0700

    part one is complete

---
+++ part-1/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('A New Simple App')
+})
+
+app.get('/api/shout/:word', (req, res) => {
+  const word = req.params.word.toUpperCase()
+  res.header({ ContentType: 'application/text' })
+  res.send(`${word}!!!`)
+})
+
+app.post('/api/array/merge', (req, res) => {
+  const {a, b} = req.body
+
+  if (!Array.isArray(a) || !Array.isArray(b)) {
+    res.status(400).send('Input data should be of type Array.')
+  }
+
+  let mergedArray = a.map((currentValue, index) => {
+    return [currentValue, b[index]]
+  }).reduce((x, y) => x.concat(y))
+
+  res.send({
+    result: mergedArray
+  })
+
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`)
+})
